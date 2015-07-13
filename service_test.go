2c73791ebc0f98db307aa9b0cf29478ff65dda5a
Mariusz Gronczewski <xani666@gmail.com>
2015-07-13 19:10:45 +0200

    service test

--- service_test.go
+++ service_test.go
@@ -4,13 +4,13 @@ import (
 	"testing"
 )
 
-func TestHost(t *testing.T) {
-	host, err := NewHostFromEnv()
+func TestService(t *testing.T) {
+	service, err := NewServiceFromEnv()
 	if err != nil {
 		t.Logf("%s", err)
 		t.FailNow()
 	}
-	if(host.Hostname == "") {
+	if(service.Hostname == "") {
 		t.Error("Empty host")
 	}
 }
