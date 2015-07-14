95532281627d8faaad2db2e092dccbcdbe06bc5c
Mariusz Gronczewski <xani666@gmail.com>
2015-07-14 19:18:59 +0200

    tests

--- host_test.go
+++ host_test.go
@@ -2,9 +2,27 @@ package nagios
 
 import (
 	"testing"
+	"os"
 )
 
-func TestHost(t *testing.T) {
+func TestMain(t *testing.T) {
+	os.Setenv("NAGIOS_HOSTNAME","testhost")
+	os.Setenv("NAGIOS_HOSTADDRESS","127.0.0.1")
+	os.Setenv("NAGIOS_HOSTDISPLAYNAME","long-test-host")
+	os.Setenv("NAGIOS_HOSTGROUPNAMES","svcgroup1,svcgroup2")
+}
+
+func TestHostBadEnv(t *testing.T) {
+//	os.Clearenv()
+	_, err := NewHostFromEnv()
+	if err == nil {
+		t.Logf("%s", err)
+		t.FailNow()
+	}
+
+}
+func TestHostFromEnv(t *testing.T) {
+
 	host, err := NewHostFromEnv()
 	if err != nil {
 		t.Logf("%s", err)
--- service_test.go
+++ service_test.go
@@ -2,9 +2,15 @@ package nagios
 
 import (
 	"testing"
+	"os"
 )
 
 func TestService(t *testing.T) {
+	os.Setenv("NAGIOS_HOSTNAME","testhost")
+	os.Setenv("NAGIOS_SERVICEDESC","test-service")
+	os.Setenv("NAGIOS_SERVICEDISPLAYNAME","test-service-name")
+	os.Setenv("NAGIOS_SERVICEGROUPNAMES","svcgroup1,svcgroup2")
+	os.Setenv("NAGIOS_SERVICEISVOLATILE","0")
 	service, err := NewServiceFromEnv()
 	if err != nil {
 		t.Logf("%s", err)
@@ -13,4 +19,15 @@ func TestService(t *testing.T) {
 	if(service.Hostname == "") {
 		t.Error("Empty host")
 	}
+}
+
+
+func TestFailIfBadEnv(t *testing.T) {
+	os.Clearenv()
+	service, err := NewServiceFromEnv()
+	if err == nil {
+		t.Log("Should fail if env is empty")
+		t.FailNow()
+	}
+	_ = service
 }
