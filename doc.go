960692aee8795959c8f920745c5bcaef27737ac0
Mariusz Gronczewski <xani666@gmail.com>
2015-08-10 18:57:51 +0200

    add jenkins script

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package nagios implements parsers and generators for various nagios functions.
+//
+// So far it implements:
+//
+// * basic Host/Service/Notification object
+// * environment parser for generating nagios objects from passed environment, to use with  notification command
