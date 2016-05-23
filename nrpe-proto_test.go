d6fb4fe8252045a105c3b54d727cfb50398b8f14
Mariusz Gronczewski <xani666@gmail.com>
2016-05-23 16:58:55 +0200

    test packet correctness

--- nrpe-proto_test.go
+++ nrpe-proto_test.go
@@ -1,7 +1,8 @@
 package nagios
 
 import (
-//	"os"
+	//	"os"
+	. "github.com/smartystreets/goconvey/convey"
 	"testing"
 	"bytes"
 	"fmt"
@@ -12,12 +13,21 @@ import (
 func TestNRPERequest(t *testing.T) {
 	var p NrpePacket
 	buf := new(bytes.Buffer)
-	p.SetMessage("hellfdfffffffffffffffffffffffffddddddddddddddddddddddddddddddddddddddddddo")
+	testStr := "=hellfdfffffddddddddddddz"
+	p.SetMessage(testStr)
 	p.PrepareRequest()
 	err := p.Generate(buf)
 	if err != nil {
 		fmt.Println("binary.Write failed:", err)
 	}
-	fmt.Printf("% x", buf.Bytes())
+	str := fmt.Sprintf("%s", buf.Bytes())
+	Convey (`create packet`,t, func() {
+		Convey("contains msg",func() {
+			So(str,ShouldContainSubstring,testStr)
+		})
+		Convey("string is nul-terminated",func() {
+			So(str,ShouldContainSubstring,testStr +"\000")
+		})
+	})
 
 }
