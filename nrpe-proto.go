5dc1ca3352aae377e012b870e58015b5c90c20fe
Mariusz Gronczewski <xani666@gmail.com>
2016-08-03 18:59:48 +0200

    few doc and format fixes

--- nrpe-proto.go
+++ nrpe-proto.go
@@ -13,6 +13,12 @@ import (
 
 const NRPE_MAX_PACKETBUFFER_LENGTH = 1024 // this length is hardcoded in nrpe.c
 // this is alphanumeric because original nrpe does the same thing, because they think it is "securer" for some wishy-washy reason
+
+// Nrpe packet types
+const (
+	NrpeTypeQuery = 1
+	NrpeTypeResponse = 2
+)
 var nrpeGarbage = []byte(`abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()'"[]{},./<>?`)
 var nrpeGarbageCont = len(nrpeGarbage)
 
@@ -39,6 +45,11 @@ type NrpePacket struct {
 	ResultCore int16
 	Buffer     [NRPE_MAX_PACKETBUFFER_LENGTH]byte
 }
+// example from check_nrpe:
+// 0000   00 02 00 01 22 c6 9f f4 38 69 63 68 65 63 6b 5f  ...."...8icheck_
+// 0010   64 69 73 6b 00 00 00 00 00 00 00 00 00 00 00 00  disk............
+// 0020   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................
+// 0030   00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................
 
 //max 1023 BYTES(not characters), it WILL truncate it if you add more
 func (r *NrpePacket) SetMessage(msg string) (err error) {
@@ -58,8 +69,9 @@ func (r *NrpePacket) SetMessage(msg string) (err error) {
 // mimic nrpe randomize_buffer
 
 // calculate crc, set version and type
+// Should be called before generating packet
 func (r *NrpePacket) PrepareRequest() (err error) {
-	r.Version = 1
+	r.Version = 2
 	r.Type = 1
 	r.Crc = 0
 	packet, err := r.GenerateBytes()
