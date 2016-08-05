8f6629269c4b05d7e471c4924a14b4e2aa25ae26
Mariusz Gronczewski <xani666@gmail.com>
2016-08-05 18:23:50 +0200

    add nrpe config parser, add rudimentary tests

---
+++ nrpe-conf.go
@@ -0,0 +1,39 @@
+package nagios
+
+import(
+	"regexp"
+	"io"
+	"bufio"
+)
+
+
+var nrpeCommandLineRe = regexp.MustCompile(`^\s*command\[(.+?)\]\s*=\s*(.+)\s*`)
+var nrpeConfigLineRe = regexp.MustCompile(`^\s*(\S+)\s*=\s*(.+)$`)
+var stripCommentRe = regexp.MustCompile(`\s*#.*`)
+
+
+type NrpeConfig struct {
+	Config map[string]string
+	Command map[string]string
+}
+
+
+func ParseNrpeConfig(data io.Reader) (cfg NrpeConfig, err error) {
+	scan := bufio.NewScanner(data)
+	cfg.Config = make(map[string]string)
+	cfg.Command = make(map[string]string)
+	for scan.Scan() {
+		line := stripCommentRe.ReplaceAllString(scan.Text(), ``)
+		matches := nrpeCommandLineRe.FindStringSubmatch(line)
+		if len(matches) > 2 {
+			cfg.Command[matches[1]] = matches[2]
+			continue
+		}
+		matches = nrpeConfigLineRe.FindStringSubmatch(line)
+		if len(matches) > 2 {
+			cfg.Config[matches[1]] = matches[2]
+			continue
+		}
+	}
+	return cfg,err
+}
---
+++ nrpe-conf_test.go
@@ -0,0 +1,18 @@
+package nagios
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+	"os"
+)
+
+func TestNrpeConf(t *testing.T) {
+	f, ferr := os.Open("t-data/nrpe.cfg")
+	cfg, err := ParseNrpeConfig(f)
+	Convey("Load file", t, func() {
+		So(ferr, ShouldEqual, nil)
+		So(err,ShouldEqual,nil)
+		So(cfg.Config["debug"],ShouldEqual,"0")
+		So(cfg.Command["check_cron"],ShouldEqual,"/usr/lib64/nagios/plugins/check_procs -a crond -c 1:10 -t 180")
+	})
+}
