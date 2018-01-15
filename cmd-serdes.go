e24764ac4f0d3ad90ca04fe531666b7fdbc6be5b
Mariusz Gronczewski <xani666@gmail.com>
2018-01-15 13:20:18 +0100

    host and service encode/decode

--- cmd-serdes.go
+++ cmd-serdes.go
@@ -5,20 +5,36 @@ import (
 	"strings"
 )
 
+
+func DecodeNagiosCmd(cmd string) (name string, args []string, err error) {
+	parts := strings.Split(cmd, ";")
+	// dumb but check if it is nagios cmd (all types of command are upcase and LOOKING_LIKE_THIS)
+	if len(parts) >= 2 && strings.ToUpper(parts[0]) == parts[0] && strings.Contains(parts[0],"_") {
+		return parts[0],parts[1:],nil
+	} else if len(parts) >= 2 {
+		return "",nil,fmt.Errorf("command [%s] doesn't look like NAGIOS_CMD", parts[0])
+	} else {
+		return "",nil,fmt.Errorf("can't parse [%s] into nagios cmd", name)
+	}
+
+}
+
+
 // Decode host check string
-func DecodeHostCheck(s string) (Host, error) {
+func DecodeHostCheck(check string) (Host, error) {
 	var parts []string
 	var h Host
-	if strings.HasPrefix(s, CmdProcessHostCheckResult) {
-		parts = strings.SplitN(s, ";", 4)
-		if len(parts) < 4 {
-			return h, fmt.Errorf("Decode error, not enough parts after splitting [%s]", s)
+	if cmd, args, err := DecodeNagiosCmd(check); err == nil && cmd == CmdProcessHostCheckResult {
+		if len(args) < 3 {
+			return h, fmt.Errorf("Decode error, not enough parts after splitting [%s]", check)
 		}
-		parts = parts[1:]
+		parts = args
+	} else if err == nil {
+		return h, fmt.Errorf("Expected host check, got [%s]",cmd)
 	} else {
-		parts = strings.SplitN(s, ";", 3)
+		parts = strings.SplitN(check, ";", 3)
 		if len(parts) < 3 {
-			return h, fmt.Errorf("Decode error, not enough parts after splitting [%s]", s)
+			return h, fmt.Errorf("Decode error, not enough parts after splitting [%s]", check)
 		}
 	}
 	h.Hostname = parts[0]
@@ -33,11 +49,49 @@ func DecodeHostCheck(s string) (Host, error) {
 
 // Encode host status into host check string (without PASSIVE_HOST_CHECK_RESULT header)
 func EncodeHostCheck(h Host) string {
-	fmt.Printf("%+v", hostStateMapNameToNum)
 	return strings.Join([]string{
 		h.Hostname,
 		hostStateMapNameToNum[h.State],
 		h.CheckMessage,
+	},";")
+
+}
+
+
+func DecodeServiceCheck(check string) (Service, error) {
+	var s Service
+	var parts []string
+	if cmd, args, err := DecodeNagiosCmd(check); err == nil && cmd == CmdProcessHostCheckResult {
+		if len(args) < 4 {
+			return s, fmt.Errorf("Decode error, not enough parts after splitting [%s]", check)
+		}
+		parts = args
+	} else if err == nil {
+		return s, fmt.Errorf("Expected host check, got [%s]",cmd)
+	} else {
+		parts = strings.SplitN(check, ";", 4)
+		if len(parts) < 4 {
+			return s, fmt.Errorf("Decode error, not enough parts after splitting [%s]", check)
+		}
+	}
+	s.Hostname = parts[0]
+	s.Description = parts[1]
+	if val, ok := serviceStateMapNumToName[parts[2]]; ok {
+		s.State = val
+	} else {
+		s.State = StateUnknown
+	}
+	s.CheckMessage = parts[3]
+	return s, nil
+
+}
+
+func EncodeServiceCheck(s Service) string {
+	return strings.Join([]string{
+		s.Hostname,
+		s.Description,
+		serviceStateMapNameToNum[s.State],
+		s.CheckMessage,
 	},";")
 
 }
--- cmd-serdes_test.go
+++ cmd-serdes_test.go
@@ -8,11 +8,76 @@ import (
 func TestHostEncode(t *testing.T) {
 	h := Host{}
 	h.Hostname = "testhost"
-	h.State = "UP"
+	h.State = StateUp
 	h.CheckMessage = "all green"
 	Convey("TestHostEncode", t, func() {
 		So(EncodeHostCheck(h), ShouldContainSubstring, "testhost;")
 		So(EncodeHostCheck(h), ShouldContainSubstring, "0;")
 		So(EncodeHostCheck(h), ShouldContainSubstring, "all green")
+	})
+}
+
+func TestHostDecode(t *testing.T) {
+	h := CmdProcessHostCheckResult + ";testhost2;1;bad"
+	host,err := DecodeHostCheck(h)
+	Convey("TestHostDecode1 - host down", t, func() {
+		So(err, ShouldBeNil)
+		So(host.Hostname, ShouldEqual, "testhost2")
+		So(host.CheckMessage, ShouldEqual, "bad")
+		So(host.State, ShouldEqual, StateDown)
+	})
+
+	h = "testhost2;1"
+	host,err = DecodeHostCheck(h)
+	Convey("TestHostDecode - bad data", t, func() {
+		So(err, ShouldNotBeNil)
+	})
+
+	h = "PROCESS_HOST_CHECK_RESULT;testhost3;2;unk"
+	host,err = DecodeHostCheck(h)
+	Convey("TestHostDecode - host unreachable", t, func() {
+		So(err, ShouldBeNil)
+		So(host.Hostname, ShouldEqual, "testhost3")
+		So(host.CheckMessage, ShouldEqual, "unk")
+		So(host.State, ShouldEqual, StateUnreachable)
+	})
+}
+
+func TestServiceEncode(t *testing.T) {
+	s := Service{}
+	s.Hostname = "testhost1"
+	s.Description = "testservice1"
+	s.CheckMessage = "check ok"
+	s.State = StateOk
+
+	Convey("TestHostEncode", t, func() {
+		So(EncodeServiceCheck(s), ShouldContainSubstring, "testhost1;")
+		So(EncodeServiceCheck(s), ShouldContainSubstring, "testservice1;")
+		So(EncodeServiceCheck(s), ShouldContainSubstring, "0;")
+		So(EncodeServiceCheck(s), ShouldContainSubstring, "check ok")
+	})
+
+}
+
+func TestServiceDecode(t *testing.T) {
+	service, err := DecodeServiceCheck(CmdProcessHostCheckResult + ";testhost2;testservice2;2;bad")
+	Convey("TestServiceDecode - service critical", t, func() {
+		So(err, ShouldBeNil)
+		So(service.Hostname, ShouldEqual, "testhost2")
+		So(service.Description, ShouldEqual, "testservice2")
+		So(service.CheckMessage, ShouldEqual, "bad")
+		So(service.State, ShouldEqual, StateCritical)
+	})
+	service,err = DecodeServiceCheck("testhost2;testserv;1")
+	Convey("TestServiceDecode - bad data", t, func() {
+		So(err, ShouldNotBeNil)
+	})
+	service, err = DecodeServiceCheck("testhost3;testservice3;0;kk")
+	Convey("TestServiceDecode - service ok", t, func() {
+		So(err, ShouldBeNil)
+		So(service.Hostname, ShouldEqual, "testhost3")
+		So(service.Description, ShouldEqual, "testservice3")
+		So(service.CheckMessage, ShouldEqual, "kk")
+		So(service.State, ShouldEqual, StateOk)
 	})
 }
